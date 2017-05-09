baa5a6e707877ae4cfb2838b280bfbaa809e11ee
fjzusi <zidane320@yahoo.com>
2017-05-09 18:10:40 -0400

    Added ui.bootstrap module

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('hive-weather', []);
+var app = angular.module('hive-weather', ['ui.bootstrap']);
 
 app.constant("apiUrl", "http://api.wunderground.com/api/d5cc774a45c7794d/");
 app.constant("autoCompleteUrl", "http://autocomplete.wunderground.com/");
