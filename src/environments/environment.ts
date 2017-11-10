e35b8a8322c4a93945d99ad1a3bbce6b848628fd
joedono <zidane320@yahoo.com>
2017-11-10 14:37:41 -0500

    Added API Urls to config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,7 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  weatherApi: "https://api.wunderground.com/api/d5cc774a45c7794d/",
+  autoCompleteApi: "https://autocomplete.wunderground.com/"
 };
