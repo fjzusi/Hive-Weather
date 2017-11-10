3e906b6b973f1a83e62a15e7caca5d7c9aa5280a
joedono <zidane320@yahoo.com>
2017-11-10 15:45:59 -0500

    Filled in conditions service

--- src/app/services/conditions.service.ts
+++ src/app/services/conditions.service.ts
@@ -1,8 +1,35 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Observable } from 'rxjs/Rx';
+import { of } from 'rxjs/observable/of';
+import { map } from 'rxjs/operators';
+
+import { Condition } from '../models/condition';
 
 @Injectable()
 export class ConditionsService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  conditionsFromLink(link: string): Observable<Condition> {
+    var url = `${environment.weatherApi}conditions${link}.json`;
+    
+    return this.getConditions(url);
+  }
+
+  conditionsFromCityState(city: string, state: string): Observable<Condition> {
+    city = city.replace(/ /gi, "_");
+    var url = `${environment.weatherApi}conditions/q/${state}/${city}.json`;
+
+    return this.getConditions(url);
+  }
+
+  private getConditions(url: string): Observable<Condition> {
+    return this.http.get<Condition>(url)
+      .pipe(
+        map(data => data.current_observation)
+      );
+  }
 
 }
