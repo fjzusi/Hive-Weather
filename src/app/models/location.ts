06167fea64df974b562942570eda87f3a6f0ce3d
joedono <zidane320@yahoo.com>
2017-11-10 14:57:36 -0500

    Added conditions models

---
+++ src/app/models/location.ts
@@ -0,0 +1,12 @@
+export class Location {
+  full: string;
+  city: string;
+  state: string;
+  state_name: string;
+  country: string;
+  country_iso3166: string;
+  zip: string;
+  latitude: float;
+  longitude: float;
+  elevation: float;
+}
