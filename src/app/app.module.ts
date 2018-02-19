d25a7b3f365ba0b7b027ee0f513a65fc9d9af707
joedono <zidane320@yahoo.com>
2018-02-19 18:07:50 -0500

    Switched to ng-bootstrap typeahead

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { Ng2AutoCompleteModule } from 'ng2-auto-complete';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 
@@ -17,8 +18,8 @@ import { SearchComponent } from './search/search.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule,
-    Ng2AutoCompleteModule
+    NgbModule.forRoot(),
+    HttpClientModule
   ],
   providers: [
     AlmanacService,
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,7 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgbTypeahead } from '@ng-bootstrap/ng-bootstrap';
+import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/merge';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
 
 import { AutocompleteService } from '../services/autocomplete.service';
 import { WU_Autocomplete_Result } from '../models/autocomplete/autocompleteresult';
+
+const states = ['Alabama', 'Alaska', 'American Samoa', 'Arizona', 'Arkansas', 'California', 'Colorado',
+  'Connecticut', 'Delaware', 'District Of Columbia', 'Federated States Of Micronesia', 'Florida', 'Georgia',
+  'Guam', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine',
+  'Marshall Islands', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana',
+  'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
+  'Northern Mariana Islands', 'Ohio', 'Oklahoma', 'Oregon', 'Palau', 'Pennsylvania', 'Puerto Rico', 'Rhode Island',
+  'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virgin Islands', 'Virginia',
+  'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
 
 @Component({
   selector: 'app-search',
@@ -9,15 +26,20 @@ import { WU_Autocomplete_Result } from '../models/autocomplete/autocompleteresul
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-  searchTerm: string;
-  searchResults: Array<WU_Autocomplete_Result>;
+  model: any;
 
-  constructor(private autocompleteService: AutocompleteService) {
-    this.searchResults = new Array<WU_Autocomplete_Result>();
-  }
+  @ViewChild('instance') instance: NgbTypeahead;
+  focus$ = new Subject<string>();
+  click$ = new Subject<string>();
 
-  search($event) {
-    console.log("thing");
+  constructor(private autocompleteService: AutocompleteService) { }
+
+  search(text$: Observable<string>) {
+    text$
+      .debounceTime(200).distinctUntilChanged()
+      .merge(this.focus$)
+      .merge(this.click$.filter(() => !this.instance.isPopupOpen()))
+      .map(term => (term === '' ? states : states.filter(v => v.toLowerCase().indexOf(term.toLowerCase()) > -1)).slice(0, 10));
   }
 
   ngOnInit() {
