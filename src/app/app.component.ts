42ee6c125ac7fa99519012517aa97c4a9c7966c0
joedono <zidane320@yahoo.com>
2018-03-22 17:56:06 -0400

    Removed extra thing...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
 
-import { WU_Autocomplete_Result } from '../models/autocomplete/autocompleteresult';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  curLocation: WU_Autocomplete_Result;
 }
